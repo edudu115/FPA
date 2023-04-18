e495bfd0d646966bd29e49cce69ee9c149ef31c6
Gustavin1501 <guvasconcelos10@gmail.com>
2023-04-18 20:37:01 -0300

    Removendo atributos do controller

--- app/Controllers/Componente.php
+++ app/Controllers/Componente.php
@@ -4,11 +4,6 @@
 
     class Componente extends BaseController
     {
-        public $idComponente;
-        public $nomeMateria;
-        public $diaHorario;
-        public $curso;
-        public $horasSemanais;
     }
 
 ?>
