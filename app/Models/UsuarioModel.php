248e5fb89941bdc934fa9fe9f75165ff400abfa3
Gustavin1501 <130517953+Gustavin1501@users.noreply.github.com>
2023-04-18 19:37:09 -0300

    adicionei o .php

---
+++ app/Models/UsuarioModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsuarioModel extends Model
+{
+    protected $table = "usuario";
+    protected $primaryKey = "idUsuario";
+    protected $useAutoIncrement = true;
+    protected $allowedFields = ["nome", "prontuario", "senha", "cpf", "cargo",
+                                 "tempoCampus", "tempoExp", 
+                                 "tempoProfissional", "tempoInstuicao", 
+                                 "nivelCarreira", "idade"];
+    protected $returnType = "array";
+
+    //gustavo é viado
+}
+?>
