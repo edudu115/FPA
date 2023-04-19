85a8b3cd2039543e5b8b05d5202bb2694ea92295
ericathalia123 <131212101+ericathalia123@users.noreply.github.com>
2023-04-18 22:23:02 -0300

    informaçaõ de horario

---
+++ app/Views/informar_hr.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title></title>
+  </head>
+
+  <body class="bg-green">
+
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"> <img class="d-block mx-auto mb-4" src="ifsp.png" alt="" width="90" height="90"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index2.php">Atualizar Cadastro  <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="informar_hr.php">Informar Disponibilidade de Horario </a>
+      </li>
+    </ul>
+  <div style='text-align:left'>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar"> 
+</div>
+<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar  </button>
+    </form>
+  </div>
+</nav> 
+<br><br>
+<center> <h4>Disponibilidade de horário para atribuição de componentes curriculares</h4> </center>
+<br><br>
+<table style="background-color:beige" class="table table-bordered">
+
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+  <label class="form-check-label" for="flexCheckDefault">
+    Default checkbox
+  </label>
+</div>
+<div class="form-check">
+  <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+  <label class="form-check-label" for="flexCheckChecked">
+    Checked checkbox
+  
+</div>
+  <thead>
+    <tr>
+      <th scope="col">Matéria</th>
+      <th scope="col">dia-Horario</th>
+      <th scope="col">curso</th> 
+      <th scope="col">Horas_Semanais</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+     
+      <td>ISO-Introdução a Sistemas Operacionais</td>
+      <td>2023-02-13 19:00:00</td>
+      <td>Tec-info noturno</td>
+      <td>4</td>
+     
+    </tr>
+    <tbody>
+    <tr>
+    
+      <td>IPW-Introdução a Programação web</td>
+      <td></td>
+      <td></td>
+    </tr>
+    
+  </tbody>
+</table>
+</label>
+</body>
+</html>
