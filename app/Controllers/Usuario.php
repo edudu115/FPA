6d5b3b9ef0cfe838d4d5ac1b4ac4bd07d877c2df
Gustavin1501 <130517953+Gustavin1501@users.noreply.github.com>
2023-04-18 22:07:52 -0300

    controller

--- app/Controllers/Usuario.php
+++ app/Controllers/Usuario.php
@@ -4,11 +4,30 @@
 
     class Usuario extends BaseController
     {
-        private $usuario
+        private $userModel;
 
         public function __construct()
         {
-            
+            $this->userModel = new \App\Models\UsuarioModel;
+        }
+
+        public function insercao()
+        {
+            $dados = [
+                "nome" => $this->request->getPost(""),
+                "prontuario" => $this->request->getPost(""),
+                "senha" => $this->request->getPost(""),
+                "cpf" => $this->request->getPost(""),
+                "cargo" => $this->request->getPost(""),
+                "tempoCampus" => $this->request->getPost(""),
+                "tempoExp" => $this->request->getPost(""),
+                "tempoProfissional" => $this->request->getPost(""),
+                "tempoInstituicao" => $this->request->getPost(""),
+                "nivelCarreira" => $this->request->getPost(""),
+                "idade" => $this->request->getPost("")
+            ];
+
+            $this->userModel->insert($dados);
         }
     }
 
