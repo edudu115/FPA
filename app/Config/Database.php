1a9380031cf8ff0f33fd802b78e7ef1ca83687a5
Gustavin1501 <guvasconcelos10@gmail.com>
2023-05-02 21:51:05 -0300

    database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'      => '',
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => 'root',
+        'password' => '',
         'database' => 'fpa_database',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
