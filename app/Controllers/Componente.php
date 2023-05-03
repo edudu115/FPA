bb611988f1a2f2e20da56f516a126efc3a29e5e9
edudu115 <edu.antoniojunior115@gmail.com>
2023-05-02 21:32:59 -0300

    alterei o componente na View e no Controller

--- app/Controllers/Componente.php
+++ app/Controllers/Componente.php
@@ -2,8 +2,20 @@
 
     namespace App\Controllers;
 
+use ReflectionFunctionAbstract;
+
     class Componente extends BaseController
     {
+        private $componenteModel;
+        public function __construct(){
+            $this->componenteModel = new \App\Models\ComponentesModel();
+        }
+
+        public function infoCadastro(){
+            return view('componentesView',[
+                                            'componetes' => $this->componenteModel->find(),
+                                          ]);
+        }
     }
 
 ?>
