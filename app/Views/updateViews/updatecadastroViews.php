5010dfdb534828ba42af7b248088031f729fe0b1
ericathalia123 <ericathalia2003@gmail.com>
2023-05-23 20:54:01 -0300

    alterações

--- app/Views/updateViews/updatecadastroViews.php
+++ app/Views/updateViews/updatecadastroViews.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html lang="pt-br">
+<html lang='pt-br'>
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <meta name='description' content=''>
+    <meta name='author' content=''>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>
 
     <title></title>
   </head>
@@ -100,32 +100,32 @@
 
     </style>
 
-  <body class="bg-green" >
+  <body class='bg-green' >
 
-  <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-green >
-  <a  id="if" class="navbar-brand" href="#"> <img class="d-block mx-auto mb-4" src="imgs/logo.png" alt="" width="500" height="90"  ></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
+  <nav id='nav' class='navbar navbar-expand-lg navbar-light bg-green >
+  <a  id='if' class='navbar-brand' href='#'> <img class='mb-4' src='<?= base_url('logo.png') ?>' alt='' width='500' height='90'  ></a>
+  <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#conteudoNavbarSuportado' aria-controls='conteudoNavbarSuportado' aria-expanded='false' aria-label='Alterna navegação'>
+    <span class='navbar-toggler-icon'></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+  <div class='collapse navbar-collapse' id='conteudoNavbarSuportado'>
    
   <div style='text-align:left'>
-    <form class="form-inline my-2 my-lg-0">
-      <input  id="pes" class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar"> 
+    <form class='form-inline my-2 my-lg-0'>
+      <input  id='pes' class='form-control mr-sm-2' type='search' placeholder='Pesquisar' aria-label='Pesquisar'> 
 </div>
-<button  id="pesq"  class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
- <a href="updateloginViews.php" <button  id="sair"  class="btn btn-outline-success my-2 my-sm-0" type="submit">sair</button></a>
+<button  id='pesq'  class='btn btn-outline-success my-2 my-sm-0' type='submit'>Pesquisar</button>
+ <a href='updateloginViews.php' <button  id='sair'  class='btn btn-outline-success my-2 my-sm-0' type='submit'>sair</button></a>
 </select>   
     </form>
   </div>
-  <a href="UsuarioViews.php"><img src="imgs/user.jpeg" class="rounded-circle" width="50" height="50"  left="50px" id="imgs"></a>
-  <a href="Alterar_SenhaViews.php"><img src="imgs/engrenagem.png" class="rounded-circle" width="50" height="50"  left="50px" id="engrenagem"></a>
+  <a href='UsuarioViews.php'><img class='mb-4' src='<?= base_url('user.jpeg') ?>' class='rounded-circle' width='50' height='50'  left='50px' id='imgs'></a>
+  <a href='Alterar_SenhaViews.php'><img class='mb-4' src='<?= base_url('engrenagem.png') ?>' class='rounded-circle' width='50' height='50'  left='50px' id='engrenagem'></a>
 </nav> 
-<div  id="bar" class="sobre">
-<nav class="nav">
-  <a id="atualiza"class="nav-link active"href="index2.php">Atualizar Cadastro</a>
-  <a id="horario"class="nav-link active"  href="informar_hr.php">Informar Disponibilidade de Horario</a>
+<div  id='bar' class='sobre'>
+<nav class='nav'>
+  <a id='atualiza'class='nav-link active'href='index2.php'>Atualizar Cadastro</a>
+  <a id='horario'class='nav-link active'  href='informar_hr.php'>Informar Disponibilidade de Horario</a>
 </nav>
       </div>
     <div class='container'>
@@ -186,7 +186,7 @@
                  Idade é obrigatório.
                 </div>
               </div>
-              <button id ="button"class='btn btn-primary btn-lg btn-block' type='submit'>Salvar</button>
+              <button id ='button'class='btn btn-primary btn-lg btn-block' type='submit'>Salvar</button>
           </form>
       </div>
             </form>
@@ -202,11 +202,11 @@
     </div>
 
     
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src='https://code.jquery.com/jquery-3.3.1.slim.min.js' integrity='sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo' crossorigin='anonymous'></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../assets/js/vendor/popper.min.js"></script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/vendor/holder.min.js"></script>
+    <script src='../../assets/js/vendor/popper.min.js'></script>
+    <script src='../../dist/js/bootstrap.min.js'></script>
+    <script src='../../assets/js/vendor/holder.min.js'></script>
     <script>
       (function() {
         'use strict';
--- app/Views/updateViews/updatecomponentesView.php
+++ app/Views/updateViews/updatecomponentesView.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html lang="pt-br">
+<html lang='pt-br'>
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <meta name='description' content=''>
+    <meta name='author' content=''>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>
 
     <title></title>
   </head>
@@ -99,47 +99,47 @@
 
     </style>
 
-  <body class="bg-green" >
+  <body class='bg-green' >
 
-  <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-green >
-  <a  id="if" class="navbar-brand" href="#"> <img class="d-block mx-auto mb-4" src="imgs/logo.png" alt="" width="500" height="90"  ></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
+  <nav id='nav' class='navbar navbar-expand-lg navbar-light bg-green >
+  <a  id='if' class='navbar-brand' href='#'> <img class='mb-4' src='<?= base_url('logo.png') ?>' alt='' width='500' height='90'  ></a>
+  <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#conteudoNavbarSuportado' aria-controls='conteudoNavbarSuportado' aria-expanded='false' aria-label='Alterna navegação'>
+    <span class='navbar-toggler-icon'></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+  <div class='collapse navbar-collapse' id='conteudoNavbarSuportado'>
    
   <div style='text-align:left'>
-    <form class="form-inline my-2 my-lg-0">
-      <input  id="pes" class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar"> 
+    <form class='form-inline my-2 my-lg-0'>
+      <input  id='pes' class='form-control mr-sm-2' type='search' placeholder='Pesquisar' aria-label='Pesquisar'> 
 </div>
-<button  id="pesq"  class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
- <a href="login.php" <button  id="sair"  class="btn btn-outline-success my-2 my-sm-0" type="submit">sair</button></a>
+<button  id='pesq'  class='btn btn-outline-success my-2 my-sm-0' type='submit'>Pesquisar</button>
+ <a href='login.php' <button  id='sair'  class='btn btn-outline-success my-2 my-sm-0' type='submit'>sair</button></a>
 </select>   
     </form>
   </div>
-  <a href="UsuarioViews.php"><img src="imgs/user.jpeg" class="rounded-circle" width="50" height="50"  left="50px" id="imgs"></a>
-  <a href="Alterar_Senha.php"><img src="imgs/engrenagem.png" class="rounded-circle" width="50" height="50"  left="50px" id="engrenagem"></a>
+  <a href='UsuarioViews.php'><img class='mb-4' src='<?= base_url('user.jpeg') ?>' class='rounded-circle' width='50' height='50'  left='50px' id='imgs'></a>
+  <a href='Alterar_Senha.php'><img class='mb-4' src='<?= base_url('engrenagem.png') ?>' class='rounded-circle' width='50' height='50'  left='50px' id='engrenagem'></a>
 </nav> 
-<div  id="bar" class="sobre">
-<nav class="nav">
-  <a id="atualiza"class="nav-link active"href="index2.php">Atualizar Cadastro</a>
-  <a id="horario"class="nav-link active"  href="informar_hr.php">Informar Disponibilidade de Horario</a>
+<div  id='bar' class='sobre'>
+<nav class='nav'>
+  <a id='atualiza'class='nav-link active'href='index2.php'>Atualizar Cadastro</a>
+  <a id='horario'class='nav-link active'  href='informar_hr.php'>Informar Disponibilidade de Horario</a>
 </nav>
       </div>
 <br><br>
 <center> <h4>Disponibilidade de horário para atribuição de componentes curriculares</h4> </center>
 <br><br>
-<table style="background-color: white" class="table table-bordered">
+<table style='background-color: white' class='table table-bordered'>
   
 </div>
   <thead>
     <tr>
    
-      <th  scope="col">Matéria</th>
-      <th scope="col">dia-Horario</th>
-      <th scope="col">curso</th> 
-      <th scope="col">Horas_Semanais</th>
+      <th  scope='col'>Matéria</th>
+      <th scope='col'>dia-Horario</th>
+      <th scope='col'>curso</th> 
+      <th scope='col'>Horas_Semanais</th>
      
      
   
@@ -153,10 +153,10 @@
       <td>2023-02-13 19:00:00 </td>
       <td>Tec-info noturno</td>
       <td>4</td>
-      <td> <select  style= "width : 150px;" class="form-select" aria-label="Default select example">
+      <td> <select  style= 'width : 150px;' class='form-select' aria-label='Default select example'>
       <option selected>Não Selecionado</option>
-  <option value="1">Primário</option>
-  <option value="2">Segundário</option>
+  <option value='1'>Primário</option>
+  <option value='2'>Segundário</option>
 </select> </td>
     </tr>
     <tbody>
@@ -167,10 +167,10 @@
       <td></td>
       <td></td>
       <th>
-      <select style= "width : 150px;" class="form-select" aria-label="Default select example" width ="40">
+      <select style= 'width : 150px;' class='form-select' aria-label='Default select example' width ='40'>
   <option selected>Não Selecionado</option>
-  <option value="1">Primário</option>
-  <option value="2">Segundário</option>
+  <option value='1'>Primário</option>
+  <option value='2'>Segundário</option>
 </select>   
     </th>
     </tr>
@@ -180,6 +180,6 @@
 </table>
 </label>
 <br><br><br>
-<button id="button" class="btn btn-primary btn-lg btn-block" type="submit">Salvar</button>
+<button id='button' class='btn btn-primary btn-lg btn-block' type='submit'>Salvar</button>
 </body>
 </html>
