dc14b8f263c22a892562a2b32850d929800626f5
ericathalia123 <ericathalia2003@gmail.com>
2023-05-23 20:24:10 -0300

    alteração

--- app/Views/updateViews/CabeçalhoViews.php
+++ app/Views/updateViews/CabeçalhoViews.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html lang="pt-br">
+<html lang='pt-br'>
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <meta name='description' content=''>
+    <meta name='author' content=''>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>
 
     <title></title>
   </head>
@@ -95,32 +95,32 @@
 
     </style>
 
-  <body class="bg-green" >
+  <body class='bg-green' >
 
-  <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-green >
-  <a  id="if" class="navbar-brand" href="#"> <img class="d-block mx-auto mb-4" src="imgs/logo.png" alt="" width="500" height="90"  ></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
+  <nav id='nav' class='navbar navbar-expand-lg navbar-light bg-green >
+  <a  id='if' class='navbar-brand' href='#'> <img class='mb-4' src='<?= base_url('logo.png') ?>' alt='' width='500' height='90'  ></a>
+  <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#conteudoNavbarSuportado' aria-controls='conteudoNavbarSuportado' aria-expanded='false' aria-label='Alterna navegação'>
+    <span class='navbar-toggler-icon'></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+  <div class='collapse navbar-collapse' id='conteudoNavbarSuportado'>
    
   <div style='text-align:left'>
-    <form class="form-inline my-2 my-lg-0">
-      <input  id="pes" class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar"> 
+    <form class='form-inline my-2 my-lg-0'>
+      <input  id='pes' class='form-control mr-sm-2' type='search' placeholder='Pesquisar' aria-label='Pesquisar'> 
 </div>
-<button  id="pesq"  class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
- <a href="updateloginViews.php" <button  id="sair"  class="btn btn-outline-success my-2 my-sm-0" type="submit">sair</button></a>
+<button  id='pesq'  class='btn btn-outline-success my-2 my-sm-0' type='submit'>Pesquisar</button>
+ <a href='updateloginViews.php' <button  id='sair'  class='btn btn-outline-success my-2 my-sm-0' type='submit'>sair</button></a>
 </select>   
     </form>
   </div>
-  <a href="UsuarioViews.php"><img src="imgs/user.jpeg" class="rounded-circle" width="50" height="50"  left="50px" id="imgs"></a>
-  <a href="Alterar_SenhaViews.php"><img src="imgs/engrenagem.png" class="rounded-circle" width="50" height="50"  left="50px" id="engrenagem"></a>
+  <a href='UsuarioViews.php'><img class='mb-4' src='<?= base_url('user.jpeg') ?>'class='rounded-circle' width='50' height='50'  left='50px' id='imgs'></a>
+  <a href='Alterar_SenhaViews.php'><img class='mb-4' src='<?= base_url('engrenagem.png') ?>' class='rounded-circle' width='50' height='50'  left='50px' id='engrenagem'></a>
 </nav> 
-<div  id="bar" class="sobre">
-<nav class="nav">
-  <a id="atualiza"class="nav-link active"href="updatecadastroViews.php">Atualizar Cadastro</a>
-  <a id="horario"class="nav-link active"  href="updatecomponentesView.php">Informar Disponibilidade de Horario</a>
+<div  id='bar' class='sobre'>
+<nav class='nav'>
+  <a id='atualiza'class='nav-link active'href='updatecadastroViews.php'>Atualizar Cadastro</a>
+  <a id='horario'class='nav-link active'  href='updatecomponentesView.php'>Informar Disponibilidade de Horario</a>
 </nav>
       </div>
 </body>
