488d1b8eeac0aa2b55dd6b7571aed50c4786bed9
ericathalia123 <ericathalia2003@gmail.com>
2023-05-23 20:20:19 -0300

    alterações

--- app/Views/updateViews/Alterar_SenhaViews.php
+++ app/Views/updateViews/Alterar_SenhaViews.php
@@ -1,11 +1,11 @@
 <!doctype html>
-<html lang="pt-br">
+<html lang='pt-br'>
   <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <meta name='description' content=''>
+    <meta name='author' content=''>
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC' crossorigin='anonymous'>
 
     <title></title>
   </head>
@@ -95,43 +95,43 @@
 
     </style>
 
-  <body class="bg-green" >
+  <body class='bg-green' >
 
-  <nav id="nav" class="navbar navbar-expand-lg navbar-light bg-green >
-  <a  id="if" class="navbar-brand" href="#"> <img class="d-block mx-auto mb-4" src="imgs/logo.png" alt="" width="500" height="90"  ></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
+  <nav id='nav' class='navbar navbar-expand-lg navbar-light bg-green >
+  <a  id='if' class='navbar-brand' href='#'> <img class='mb-4' src='<?= base_url('logo.png') ?>' alt='' width='500' height='90' ></a>
+  <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#conteudoNavbarSuportado' aria-controls='conteudoNavbarSuportado' aria-expanded='false' aria-label='Alterna navegação'>
+    <span class='navbar-toggler-icon'></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+  <div class='collapse navbar-collapse' id='conteudoNavbarSuportado'>
    
   <div style='text-align:left'>
-    <form class="form-inline my-2 my-lg-0">
-      <input  id="pes" class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar"> 
+    <form class='form-inline my-2 my-lg-0'>
+      <input  id='pes' class='form-control mr-sm-2' type='search' placeholder='Pesquisar' aria-label='Pesquisar'> 
 </div>
-<button  id="pesq"  class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
- <a href="login.php" <button  id="sair"  class="btn btn-outline-success my-2 my-sm-0" type="submit">sair</button></a>
+<button  id='pesq'  class='btn btn-outline-success my-2 my-sm-0' type='submit'>Pesquisar</button>
+ <a href='login.php' <button  id='sair'  class='btn btn-outline-success my-2 my-sm-0' type='submit'>sair</button></a>
 </select>   
     </form>
   </div>
-  <a href="UsuarioViews.php"><img src="imgs/user.jpeg" class="rounded-circle" width="50" height="50"  left="50px" id="imgs"></a>
-  <a href="Alterar_Senha.php"><img src="imgs/engrenagem.png" class="rounded-circle" width="50" height="50"  left="50px" id="engrenagem"></a>
+  <a href='UsuarioViews.php'><img class='mb-4' src='<?= base_url('user.jpeg') ?>' class='rounded-circle' width='50' height='50'  left='50px' id='imgs'></a>
+  <a href='Alterar_Senha.php'><img class='mb-4' src='<?= base_url('engrenagem.png') ?>' class='rounded-circle' width='50' height='50'  left='50px' id='engrenagem'></a>
 </nav> 
-<div  id="bar" class="sobre">
-<nav class="nav">
-  <a id="atualiza"class="nav-link active"href="index2.php">Atualizar Cadastro</a>
-  <a id="horario"class="nav-link active"  href="informar_hr.php">Informar Disponibilidade de Horario</a>
+<div  id='bar' class='sobre'>
+<nav class='nav'>
+  <a id='atualiza'class='nav-link active'href='index2.php'>Atualizar Cadastro</a>
+  <a id='horario'class='nav-link active'  href='informar_hr.php'>Informar Disponibilidade de Horario</a>
 </nav>
       </div>
       <br><br>
     <title>Toggle Password Visibility</title>
-    <link rel="stylesheet" href=
-"https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
-    <link rel="stylesheet" href=
-"https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    <link rel='stylesheet' href=
+'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css' />
+    <link rel='stylesheet' href=
+'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css'
         integrity=
-"sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-        crossorigin="anonymous">
+'sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T'
+        crossorigin='anonymous'>
   
     <style>
 
@@ -156,7 +156,7 @@
   
 <body>
     <div class='text-center'>
-    <img src="imgs/senha.jpeg" class="rounded-circle" width="90" height="95"  left="50px" id="user">
+    <img class='mb-4' src='<?= base_url('senha.jpeg') ?>' class='rounded-circle' width='90' height='95'  left='50px' id='user'>
     <br><br>
        <h1>Alteração de senha</h1>
        <br><br>
@@ -164,28 +164,28 @@
         
             <p>
                 <label>Digite a senha atual</label>
-                <input type="password" 
-                    name="password" id="password" />
-                <i class="bi bi-eye-slash" 
-                    id="togglePassword"></i>
+                <input type='password' 
+                    name='password' id='password' />
+                <i class='bi bi-eye-slash' 
+                    id='togglePassword'></i>
             </p>
             <p>
                 <label>Nova senha</label>
-                <input type="password" 
-                    name="password" id="password" />
-                <i class="bi bi-eye-slash" 
-                    id="togglePassword"></i>
+                <input type='password' 
+                    name='password' id='password' />
+                <i class='bi bi-eye-slash' 
+                    id='togglePassword'></i>
             </p>
             <p>
                 <label>Confirme a senha</label>
-                <input type="password" 
-                    name="password" id="password" />
-                <i class="bi bi-eye-slash" 
-                    id="togglePassword"></i>
+                <input type='password' 
+                    name='password' id='password' />
+                <i class='bi bi-eye-slash' 
+                    id='togglePassword'></i>
             </p>
   
-            <button type="submit" id="submit" 
-                class="btn btn-primary">
+            <button type='submit' id='submit' 
+                class='btn btn-primary'>
                 Confirmar 
             </button>
         </form>
