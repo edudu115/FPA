9dd8a313b70c51cd561bda15f41ddfe164ed2290
edudu115 <edu.antoniojunior115@gmail.com>
2023-05-24 21:44:14 -0300

    form

--- app/Controllers/Componentes.php
+++ app/Controllers/Componentes.php
@@ -41,9 +41,21 @@
 
             $this->componenteModel->insert($dados);
 
-            $vetor = ["idComponente" => $this->request->getPost('idComponente')];
-            return view('nome', $vetor);
+            $id = ["idComponente" => $this->request->getPost('idComponente')];
+            return view('form2ComponentesView', $id);
             }
+
+        public function saveHorario(){
+            $dados2 = [
+                'componente_idComponentes'=>$this->request->getPost('id'),
+                'diaSemana'=>$this->request->getPost('diaSemana'),
+                'horaInicio'=>$this->request->getPost('horaInicio'),
+                'horaFim'=>$this->request->getPost('nomeMateria')
+            ];
+            //var_dump($dados2);
+            $this->horarioModel->insert($dados2);
+            return view('componentesView');
+        }
 
         public function deleteComponente($id){
             $this->horarioModel->deleteHorario($id);
