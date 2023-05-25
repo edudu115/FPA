0d60bdb3d5d7cf0eba25ebf9d1ebacc8d581bd23
ericathalia123 <ericathalia2003@gmail.com>
2023-05-24 22:13:56 -0300

    alterações

--- app/Views/cadastroView.php
+++ app/Views/cadastroView.php
@@ -1,41 +1,8 @@
-<!doctype html>
-<html lang="pt-br">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<?php $this->extend('base');
+  $this->section('corpo');
+?>
 
-    <title></title>
-  </head>
-
-  <body class="bg-green">
-
-   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href=""> <img class='mb-4' src='<?= base_url('ifsp.png') ?>' alt='' width='90' height='90'></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#conteudoNavbarSuportado" aria-controls="conteudoNavbarSuportado" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Atualizar Cadastro  <span class="sr-only"></span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="informar_hr.php">Informar Disponibilidade de Horario </a>
-      </li>
-    </ul>
-  <div style='text-align:left'>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar"> 
-</div>
-<button class="btn btn-outline-success my-2 my-sm-0" type= "submit">Pesquisar  </button>
-    </form>
-  </div>
-</nav> 
-
+  
     <div class="container">
       <div class="py-1 text-center">
 
@@ -137,7 +104,6 @@
         window.addEventListener('load', function() {
           
           var forms = document.getElementsByClassName('needs-validation');
-
         
           var validation = Array.prototype.filter.call(forms, function(form) {
             form.addEventListener('submit', function(event) {
@@ -151,5 +117,4 @@
         }, false);
       })();
     </script>
-  </body>
-</html>
+    <?php $this->endSection(); ?>
