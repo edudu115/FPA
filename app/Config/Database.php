e264d011bd3773192c2ca2f3dd8b3f7ed1bcbd29
edudu115 <edu.antoniojunior115@gmail.com>
2023-05-30 20:06:37 -0300

    Desfazendo a caca do Gustavo

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,9 +26,9 @@ class Database extends Config
      */
     public array $default = [
         'DSN'      => '',
-        'hostname' => '127.0.0.1',
+        'hostname' => 'localhost',
         'username' => 'root',
-        'password' => 'root',
+        'password' => 'ro311280',
         'database' => 'fpa_database',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
@@ -41,7 +41,7 @@ class Database extends Config
         'compress' => false,
         'strictOn' => false,
         'failover' => [],
-        'port'     => 3307,
+        'port'     => 3306,
     ];
 
     /**
--- app/Models/PreferenciaModel.php
+++ app/Models/PreferenciaModel.php
@@ -27,4 +27,4 @@ class PreferenciaModel extends Model{
     }
 }
 
-?>
+?>  
