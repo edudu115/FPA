67dae3dd9d17d970176acb03d58eb2db41ce47f6
LucasAlt40 <lucas.alt40@gmail.com>
2023-05-30 20:26:19 -0300

    Padronizando projeto

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -28,7 +28,7 @@ class Database extends Config
         'DSN'      => '',
         'hostname' => 'localhost',
         'username' => 'root',
-        'password' => 'ro311280',
+        'password' => '',
         'database' => 'fpa_database',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
