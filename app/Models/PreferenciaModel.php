392364e660dd6d4f1c741f0e1c94ced0888639aa
edudu115 <edu.antoniojunior115@gmail.com>
2023-06-14 22:22:03 -0300

    preferencia

--- app/Models/PreferenciaModel.php
+++ app/Models/PreferenciaModel.php
@@ -4,16 +4,20 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class PreferenciaModel extends Model{
+class PreferenciaModel extends Model
+{
     protected $table = "preferencia";
     protected $primaryKey = "idPreferencia";
     protected $useAutoIncrement = true;
-    protected $allowedFields =  ["usuario_idUsuario", 
-                                "componentes_idComponentes",
-                                "prioridade"];
+    protected $allowedFields =  [
+        "usuario_idUsuario",
+        "componentes_idComponentes",
+        "prioridade"
+    ];
     protected $returnType = "object";
 
-    public function selecPreferencia(){
+    public function selecPreferencia()
+    {
         $query = $this->db->query("SELECT u.nome, c.nomeMateria, p.prioridade
                                    FROM ((preferencia p 
                                    INNER JOIN usuario u ON u.idUsuario = p.usuario_idUsuario)
@@ -21,12 +25,19 @@ class PreferenciaModel extends Model{
         return $query->getResult('object');
     }
 
-    public function repeticaoPreferencia($materia, $prioridade){
+    public function repeticaoPreferencia($materia, $prioridade)
+    {
         $query = $this->db->query("SELECT componentes_idComponentes, usuario_idUsuario, prioridade FROM preferencia 
-                                    WHERE componentes_idComponentes = '".$materia."' 
-                                    AND prioridade = '".$prioridade."';");
+                                   WHERE componentes_idComponentes = '" . $materia . "' 
+                                   AND prioridade = '" . $prioridade . "';");
+        return $query->getResult('object');
+    }
+
+    public function contMateria($idMateria)
+    {
+        $query = $this->db->query("SELECT COUNT(usuario_idUsuario) AS quantidade_preferem, componentes_idComponentes, prioridade
+                                   FROM preferencia
+                                   GROUP BY prioridade, componentes_idComponentes;");
         return $query->getResult('object');
     }
 }
-
-?>
--- app/Views/prefenciaView.php
+++ app/Views/prefenciaView.php
@@ -5,55 +5,56 @@ $session = session();
 
 
 <style>
+    #la {
+        width: 50px;
 
-#la{
-    width: 50px;
-    
-   
-}
-#le{
-    width: 50px;
-   
-   
-}
-#lu{
-    width: 50px;
-   
-}
 
-#button{
+    }
 
-    background-color: green;
-}
+    #le {
+        width: 50px;
+
+
+    }
+
+    #lu {
+        width: 50px;
+
+    }
+
+    #button {
+
+        background-color: green;
+    }
 </style>
 
 
 
 <br><br>
-   <table style="background-color: white" class="table table-bordered">
-        <thead>
-            <tr>
-                <th id='la' scope="col">Nome do professor</th>
-                <th id='le'  scope="col">Nome da Matéria</th>
-                <th id='lu' scope="col">Prioridade</th>
-                 </tr> 
-        </thead>
-        <tbody>
-                <?php
-                $i = 0;
-                foreach($retorna as $preferencia){
-                echo "<tr>";
-                //echo "<input type='hidden' name='id_prefe$preferencia' value='$preferencia->idprefe$preferencias'";
-                    echo "<td>".$preferencia->nome."</td>";
-                    echo "<td>".$preferencia->nomeMateria."</td>";
-                    if($preferencia->prioridade == "1"){
-                        echo "<td>Primária</td>";
-                    }else{
-                        echo "<td>Secundário</td>";
-                    }
-                }
-                ?>
-        </tbody>
-    </table>
-    <a id="button" class='btn btn-success btn-lg btn-block' href='<?= base_url('Componentes/viewFormComponente') ?>'>Gerar horário</a>
-    <?php $this->endSection(); ?>
+<table style="background-color: white" class="table table-bordered">
+    <thead>
+        <tr>
+            <th id='la' scope="col">Nome do professor</th>
+            <th id='le' scope="col">Nome da Matéria</th>
+            <th id='lu' scope="col">Prioridade</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        $i = 0;
+        foreach ($retorna as $preferencia) {
+            echo "<tr>";
+            //echo "<input type='hidden' name='id_prefe$preferencia' value='$preferencia->idprefe$preferencias'";
+            echo "<td>" . $preferencia->nome . "</td>";
+            echo "<td>" . $preferencia->nomeMateria . "</td>";
+            if ($preferencia->prioridade == "1") {
+                echo "<td>Primária</td>";
+            } else {
+                echo "<td>Secundário</td>";
+            }
+        }
+        ?>
+    </tbody>
+</table>
+<a id="button" class='btn btn-success btn-lg btn-block' href='<?= base_url('Atribuicoes/') ?>'>Gerar horário</a>
+<?php $this->endSection(); ?>
