2a0eab3c67f7c9000e06752d9efc83b1fd058689
LDonadi <lucasdonadi58@gmail.com>
2023-06-20 20:17:20 -0300

    search

--- app/Views/atribuidoView.php
+++ app/Views/atribuidoView.php
@@ -2,10 +2,47 @@
 $this->section('corpo');
 $session = session();
 ?>
+<script>
+    function search_materia() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('componentes');
+
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].textContent.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+          x[i].style.display="";
+        }
+    } 
+}
+
+  </script>
 <style>
     body {
         background-color: #c2ddc1;
     }
+    #searchbar{
+     margin-left: 5%;
+     padding:15px;
+     border-radius: 10px;
+   }
+ 
+   input[type=text] {
+      width: 18%;
+      -webkit-transition: width 0.15s ease-in-out;
+      transition: width 0.15s ease-in-out;
+   }
+ 
+   input[type=text]:focus {
+     width: 30%;
+   }
+ 
+  #list{
+    font-size:  1.5em;
+    margin-left: 90px;
+   }
 </style>
 <link rel='icon' href='../../../../favicon.ico'>
 
@@ -19,12 +56,15 @@ $session = session();
 
 <body class='text-center'>
     <br /> <br />
+
     <center>
         <h1 class="display-4">Tabela de Atribuição de Aulas</h1>
     </center>
+    <input class="form-control mr-sm-2" id="searchbar" onkeyup="search_materia()" type="text" placeholder="Pesquisar por Professor ...">
+
     <br />
     <div class="container">
-        <table class="table table-hover table-sm">
+        <table class="table table-hover table-sm" class="list">
             <thead>
                 <tr>
                     <th scope="col">Nome do Prefessor</th>
@@ -34,7 +74,7 @@ $session = session();
                 </tr>
             </thead>
             <?php foreach ($retorna as $atribuido) : ?>
-                <tr>
+                <tr class='componentes'>
                     <td><?php echo $atribuido->nome; ?></td>
                     <td><?php echo $atribuido->sigla; ?></td>
                     <td><?php echo $atribuido->nomeMateria; ?></td>
--- app/Views/prefenciaView.php
+++ app/Views/prefenciaView.php
@@ -2,18 +2,56 @@
 $this->section('corpo');
 $session = session();
 ?>
+  <script>
+    function search_materia() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('componentes');
 
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].textContent.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+          x[i].style.display="";
+        }
+    } 
+}
 
+  </script>
 <style>
     #button {
 
         background-color: green;
     }
+    #searchbar{
+     margin-left: 5%;
+     padding:15px;
+     border-radius: 10px;
+   }
+ 
+   input[type=text] {
+      width: 18%;
+      -webkit-transition: width 0.15s ease-in-out;
+      transition: width 0.15s ease-in-out;
+   }
+ 
+   input[type=text]:focus {
+     width: 30%;
+   }
+ 
+  #list{
+    font-size:  1.5em;
+    margin-left: 90px;
+   }
 </style>
 
 <br><br>
+
+<input class="form-control mr-sm-2" id="searchbar" onkeyup="search_materia()" type="text" placeholder="Pesquisar por Professor ...">
+
 <div class="container">
-    <table style="background-color: white" class="table table-hover table-sm">
+    <table style="background-color: white" class="table table-hover table-sm" class="list">
         <thead>
             <tr>
                 <th scope="col">Nome do professor</th>
@@ -26,7 +64,7 @@ $session = session();
             <?php
             $i = 0;
             foreach ($retorna as $preferencia) {
-                echo "<tr>";
+                echo "<tr class='componentes'>";
                 //echo "<input type='hidden' name='id_prefe$preferencia' value='$preferencia->idprefe$preferencias'";
                 echo "<td>" . $preferencia->nome . "</td>";
                 echo "<td>" . $preferencia->idComponentes . "</td>";
