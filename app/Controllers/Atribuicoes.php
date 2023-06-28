5e6cc5bc84fcbb5cfbef308424849df1cdea6373
Gustavin1501 <guvasconcelos10@gmail.com>
2023-06-28 19:33:36 -0300

    resolvendo probrema

--- app/Controllers/Atribuicoes.php
+++ app/Controllers/Atribuicoes.php
@@ -92,7 +92,7 @@
 
             if(sizeof($preferencias) >= 1)
             {
-                $auxiliar = ['idUsuario' => 0,'tempoCampus' => -1,
+                $auxiliar = ['idUsuario' => null,'tempoCampus' => -1,
                 'tempoExp' => -1,'tempoProfissional' => -1,
                 'tempoInstituicao' => -1, 
                 'nivelCarreira' => -1 , 'idade' => -1];
